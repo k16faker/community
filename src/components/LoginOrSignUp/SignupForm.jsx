35d650bdb70067aae4f96c325056f75be77e901e
ParkSeongJun <sjyj98@naver.com>
2024-04-02 16:26:43 +0900

    회원가입 기능 구현

--- src/components/LoginOrSignUp/SignupForm.jsx
+++ src/components/LoginOrSignUp/SignupForm.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
 
 const SignupForm = () => {
   const navigate = useNavigate();
@@ -7,14 +8,35 @@ const SignupForm = () => {
     navigate("/login");
   };
 
+  const { user, signUp} = UserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const signUpHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password, nickname);
+      if(email === "" || password === "" || nickname === ""){
+        alert("모든 항목을 입력해주세요.");
+        return;
+      };
+      alert("회원가입이 완료되었습니다.");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   
 
   return (
     <section className="w-[800px] mx-auto mt-[100px]  text-center">
-      <form className="w-[500px] mx-auto border-2 border-purple-700 py-4">
+      <form onSubmit={signUpHandler} className="w-[500px] mx-auto border-2 border-purple-700 py-4">
         <div>
           <div className="p-1">
             <input
+            onChange={(e) => setNickname(e.target.value)}
               className="border border-black p-1 w-[250px]"
               type="text"
               placeholder="닉네임"
@@ -22,13 +44,15 @@ const SignupForm = () => {
           </div>
           <div className="p-1">
             <input
+              onChange={(e) => setEmail(e.target.value)}
               className="border border-black p-1 w-[250px]"
-              type="text"
-              placeholder="아이디"
+              type="email"
+              placeholder="이메일"
             />
           </div>
           <div className="p-1">
             <input
+              onChange={(e) => setPassword(e.target.value)}
               className="border border-black p-1 w-[250px]"
               type="password"
               placeholder="비밀번호"
